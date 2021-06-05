d289041fa8047f053286afd2488d724436a370c3
Xuwei Li <code@xuwei.li>
2021-06-05 13:26:48 +0800

    Add Font Awesome packages. Add github repository link to PageFooterComponent.

--- src/components/page-footer--back-to-homepage.js
+++ src/components/page-footer--back-to-homepage.js
@@ -1,5 +1,8 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // Styles
 const pageFooterStyles = {
@@ -11,13 +14,16 @@ const copyrightStyles = {
   display: "inline-block",
   float: "right",
 }
+// Styles: Font Awesome
+library.add(fab)
+
 
 // Markup
 const PageFooterComponent = () => {
   return (
     <div style={pageFooterStyles}>
       <p style={backToHomepageStyles}><Link to="/">Back to homepage</Link></p>
-      <p style={copyrightStyles}>Copyright &copy; {new Date().getFullYear()} V4EX Inc.</p>
+      <p style={copyrightStyles}><a href="https://github.com/v4ex/www.v4ex.com/tree/v1.0"><FontAwesomeIcon icon={["fab", "github"]} /></a> Copyright &copy; {new Date().getFullYear()} V4EX Inc.</p>
     </div>
   )
 }
--- src/components/page-footer.js
+++ src/components/page-footer.js
@@ -1,15 +1,20 @@
 import * as React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // Styles
 const pageFooterStyles = {
   textAlign: "right",
 }
+// Styles: Font Awesome
+library.add(fab)
 
 // Markup
 const PageFooterComponent = () => {
   return (
     <div style={pageFooterStyles}>
-      <p>Copyright &copy; {new Date().getFullYear()} V4EX Inc.</p>
+      <p><a href="https://github.com/v4ex/www.v4ex.com/tree/v1.0"><FontAwesomeIcon icon={["fab", "github"]} /></a> Copyright &copy; {new Date().getFullYear()} V4EX Inc.</p>
     </div>
   )
 }
