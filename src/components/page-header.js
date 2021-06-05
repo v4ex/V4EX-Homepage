a40bbc4b6832203f522ff2559332e05994f77108
Xuwei Li <code@xuwei.li>
2021-06-05 12:52:11 +0800

    Update address information in PageHeaderComponent.

--- src/components/page-header.js
+++ src/components/page-header.js
@@ -10,8 +10,8 @@ const PageHeaderComponent = () => {
   return (
     <div style={pageHeaderStyles}>
       <h1>V4EX Inc.</h1>
-      <p>651 N Broad St, Suite 206</p>
-      <p>Middletown, DE 19709</p>
+      <p>651 North Broad Steet, Suite 206</p>
+      <p>Middletown, DE 19709, USA</p>
       <p>Official Homepage</p>
     </div>
   )
