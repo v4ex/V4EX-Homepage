19a899f38b68a9a9318f3f3d0dd6fe4e55e98630
Xuwei Li <code@xuwei.li>
2021-06-09 14:38:12 +0800

    Quote style fix.

--- src/components/partial-newsletter.js
+++ src/components/partial-newsletter.js
@@ -19,13 +19,13 @@ const formStyles = {
 }
 
 const MyButton = styled(Button)({
-  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
   border: 0,
   borderRadius: 3,
-  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  color: 'white',
-  padding: '0 30px',
-  marginLeft: '1em',
+  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  color: "white",
+  padding: "0 30px",
+  marginLeft: "1em",
 })
 
 const resultStyles = {
