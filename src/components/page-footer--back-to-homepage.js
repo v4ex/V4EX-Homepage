994aa1ff425901110ada894acfdd1aecb9cb0800
Xuwei Li <code@xuwei.li>
2021-06-11 11:21:53 +0800

    Update Github repository link in page footer.

--- src/components/page-footer--back-to-homepage.js
+++ src/components/page-footer--back-to-homepage.js
@@ -1,8 +1,10 @@
+// Import
 import * as React from "react"
 import { Link } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+
 
 // Styles
 const pageFooterStyles = {
@@ -23,10 +25,11 @@ const PageFooterComponent = () => {
   return (
     <div style={pageFooterStyles}>
       <p style={backToHomepageStyles}><Link to="/">Back to homepage</Link></p>
-      <p style={copyrightStyles}><a href="https://github.com/v4ex/V4EX-Company"><FontAwesomeIcon icon={["fab", "github"]} /></a> Copyright &copy; {new Date().getFullYear()} V4EX Inc.</p>
+      <p style={copyrightStyles}><a href="https://github.com/v4ex/V4EX-Homepage"><FontAwesomeIcon icon={["fab", "github"]} /></a> Copyright &copy; {new Date().getFullYear()} V4EX Inc.</p>
     </div>
   )
 }
 
 
+// Export
 export default PageFooterComponent
--- src/components/page-footer.js
+++ src/components/page-footer.js
@@ -1,7 +1,9 @@
+// Import
 import * as React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+
 
 // Styles
 const pageFooterStyles = {
@@ -10,14 +12,16 @@ const pageFooterStyles = {
 // Styles: Font Awesome
 library.add(fab)
 
+
 // Markup
 const PageFooterComponent = () => {
   return (
     <div style={pageFooterStyles}>
-      <p><a href="https://github.com/v4ex/V4EX-Company"><FontAwesomeIcon icon={["fab", "github"]} /></a> Copyright &copy; {new Date().getFullYear()} V4EX Inc.</p>
+      <p><a href="https://github.com/v4ex/V4EX-Homepage"><FontAwesomeIcon icon={["fab", "github"]} /></a> Copyright &copy; {new Date().getFullYear()} V4EX Inc.</p>
     </div>
   )
 }
 
 
+// Export
 export default PageFooterComponent
